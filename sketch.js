a7575c7b75ebcdccfb1022dfe7e8da9eab9a6c6a
ritwiksaraf28062008 <62437795+ritwiksaraf28062008@users.noreply.github.com>
2020-05-03 12:35:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let detailY;
+// slide to see how detailY works
+function setup() {
+  createCanvas(1400, 800);
+}
+
+function draw() {
+  background(3,3,3);
+//sun  
+fill(225,123,0);
+circle(1150,400,400);
+
+//mercury
+fill(133,116,104);
+circle(845,380,30);
+
+//venus
+fill(159,176,180);
+circle(795,340,60);
+
+//earth
+fill(40,62,131);
+circle(735,420,75);
+
+
+//mars
+fill(246,127,95);
+circle(700,300,45);
+
+//jupiter
+fill(224,177,133);
+circle(650,550,160);
+
+//saturn
+fill(155,140,107);
+circle(550,140,130);
+
+//uranus
+fill(200,238,241);
+circle(400,300,105);
+
+//neptune
+fill(105,19,175);
+circle(200,400,90);
+
+}
